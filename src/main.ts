e2d2ca7bea4f397dc36fbbfa2516cfe0f324e795
IsmaelTerreno <elismakof2002@gmail.com>
2024-03-16 13:17:40 -0300

    Add dotenv, class-transformer, class-validator and improve server configuration
    This commit introduces three new dependencies: dotenv, class-transformer, and class-validator. The dotenv library is used to manage environment variables, while class-transformer and class-validator are used for DTO validation and transformation. The server bootstrap function in main.ts has been updated: CORS is now configurable through environment variables, the server port is now configurable, and a global validation pipe has been introduced, enforcing strict validation on all incoming requests.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './wages-processor/app.module';
+import * as dotenv from 'dotenv';
+import * as process from 'process';
+import { ValidationPipe } from "@nestjs/common";
+
+dotenv.config();
+
+/**
+ * Gets the cors origin to respect the expressjs cors which is behind scenes in NestJs
+ * @return Array[string]
+ * @link https://github.com/expressjs/cors#configuration-options
+ */
+export const getCORSConfig = () => {
+  const CORS_ALLOW_ORIGIN = process.env.CORS_ALLOW_ORIGIN;
+  const ALLOWED_ORIGINS = CORS_ALLOW_ORIGIN.split(',');
+  return ALLOWED_ORIGINS;
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    rawBody: true,
+    logger: ['error', 'warn', 'debug', 'log'],
+    cors: {
+      origin: getCORSConfig(),
+      credentials: true,
+    },
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(parseInt(process.env.SERVER_PORT_LISTENING) || 6030);
 }
 bootstrap();
